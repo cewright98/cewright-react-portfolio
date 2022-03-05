35f9d4941c62266bbe1d96da2e4458daabc66cf1
Claire <cewright1998@gmail.com>
2022-03-05 15:37:47 -0500

    filled out portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,51 @@
 function Portfolio() {
   // return JSX with 6 of my applications with deployed and github links
+  return (
+    <section>
+      <div>
+        <img src="/weather-dashboard.png" alt="Weather Dashboard Preview"></img>
+        <a href="https://cewright98.github.io/weather-dashboard/" target="_blank">Weather Dashboard</a>
+        <a href="https://github.com/cewright98/weather-dashboard" target="_blank">
+          <i class="fa fa-github"></i>
+        </a>
+      </div>
+      <div>
+        <img src="/note-taker.png" alt="Note Taker Preview"></img>
+        <a href="https://glacial-plateau-16713.herokuapp.com/" target="_blank">Express Note Taker</a>
+        <a href="https://github.com/cewright98/express-note-taker" target="_blank">
+          <i class="fa fa-github"></i>
+        </a>
+      </div>
+      <div>
+        <img src="/GamePlaylist.png" alt="GamePlaylist Preview"></img>
+        <a href="https://cewright98.github.io/GAMEPLAYlist/" target="_blank">GamePlaylist</a>
+        <a href="https://github.com/cewright98/GAMEPLAYlist" target="_blank">
+          <i class="fa fa-github"></i>
+        </a>
+      </div>
+      <div>
+        <img src="/StarGazr.png" alt="StarGazr Preview"></img>
+        <a href="https://sleepy-beyond-47602.herokuapp.com/" target="_blank">StarGazr</a>
+        <a href="https://github.com/erinnaschneider/stargazr" target="_blank">
+          <i class="fa fa-github"></i>
+        </a>
+      </div>
+      <div>
+        <img src="/work-day-scheduler.png" alt="Work Day Scheduler Preview"></img>
+        <a href="https://cewright98.github.io/work-day-scheduler//" target="_blank">Work Day Scheduler</a>
+        <a href="https://github.com/cewright98/work-day-scheduler" target="_blank">
+          <i class="fa fa-github"></i>
+        </a>
+      </div>
+      <div>
+        <img src="/e-commerce-database.png" alt="E-Commerce Database Preview"></img>
+        <a href="https://github.com/cewright98/e-commerce-database" target="_blank">Express Note Taker</a>
+        <a href="https://github.com/cewright98/e-commerce-database" target="_blank">
+          <i class="fa fa-github"></i>
+        </a>
+      </div>
+    </section>
+  )
 }
 
 export default Portfolio;
