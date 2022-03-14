ebc47bfb2719d4e25bedebda0494c25a58b3fad3
Claire <cewright1998@gmail.com>
2022-03-14 19:57:50 -0400

    put portfolio in a grid for desktop

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,64 +10,68 @@ function Portfolio() {
   return (
     <section className="portfolio">
       <h2>My Portfolio</h2>
-      <div className="container">
-        <img src={photo1} alt="Weather Dashboard Preview"></img>
-        <div className="container-text">
-          <a href="https://cewright98.github.io/weather-dashboard/" target="_blank">Weather Dashboard</a>
-          <br />
-          <a href="https://github.com/cewright98/weather-dashboard" target="_blank">
-            <i className="fa fa-github"></i>
-          </a>
+      <div className='row'>
+        <div className="container">
+          <img src={photo1} alt="Weather Dashboard Preview"></img>
+          <div className="container-text">
+            <a href="https://cewright98.github.io/weather-dashboard/" target="_blank">Weather Dashboard</a>
+            <br />
+            <a href="https://github.com/cewright98/weather-dashboard" target="_blank">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+        <div className="container">
+          <img src={photo2} alt="Note Taker Preview"></img>
+          <div className="container-text">
+            <a href="https://glacial-plateau-16713.herokuapp.com/" target="_blank">Express Note Taker</a>
+            <br />
+            <a href="https://github.com/cewright98/express-note-taker" target="_blank">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+        <div className="container">
+          <img src={photo3} alt="GamePlaylist Preview"></img>
+          <div className="container-text">
+            <a href="https://cewright98.github.io/GAMEPLAYlist/" target="_blank">GamePlaylist</a>
+            <br />
+            <a href="https://github.com/cewright98/GAMEPLAYlist" target="_blank">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
         </div>
       </div>
-      <div className="container">
-        <img src={photo2} alt="Note Taker Preview"></img>
-        <div className="container-text">
-          <a href="https://glacial-plateau-16713.herokuapp.com/" target="_blank">Express Note Taker</a>
-          <br />
-          <a href="https://github.com/cewright98/express-note-taker" target="_blank">
-            <i className="fa fa-github"></i>
-          </a>
+      <div className='row'>
+        <div className="container">
+          <img src={photo4} alt="StarGazr Preview"></img>
+          <div className="container-text">
+            <a href="https://sleepy-beyond-47602.herokuapp.com/" target="_blank">StarGazr</a>
+            <br />
+            <a href="https://github.com/erinnaschneider/stargazr" target="_blank">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="container">
-        <img src={photo3} alt="GamePlaylist Preview"></img>
-        <div className="container-text">
-          <a href="https://cewright98.github.io/GAMEPLAYlist/" target="_blank">GamePlaylist</a>
-          <br />
-          <a href="https://github.com/cewright98/GAMEPLAYlist" target="_blank">
-            <i className="fa fa-github"></i>
-          </a>
+        <div className="container">
+          <img src={photo5} alt="Work Day Scheduler Preview"></img>
+          <div className="container-text">
+            <a href="https://cewright98.github.io/work-day-scheduler//" target="_blank">Work Day Scheduler</a>
+            <br />
+            <a href="https://github.com/cewright98/work-day-scheduler" target="_blank">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="container">
-        <img src={photo4} alt="StarGazr Preview"></img>
-        <div className="container-text">
-          <a href="https://sleepy-beyond-47602.herokuapp.com/" target="_blank">StarGazr</a>
-          <br />
-          <a href="https://github.com/erinnaschneider/stargazr" target="_blank">
-            <i className="fa fa-github"></i>
-          </a>
-        </div>
-      </div>
-      <div className="container">
-        <img src={photo5} alt="Work Day Scheduler Preview"></img>
-        <div className="container-text">
-          <a href="https://cewright98.github.io/work-day-scheduler//" target="_blank">Work Day Scheduler</a>
-          <br />
-          <a href="https://github.com/cewright98/work-day-scheduler" target="_blank">
-            <i className="fa fa-github"></i>
-          </a>
-        </div>
-      </div>
-      <div className="container">
-        <img src={photo6} alt="E-Commerce Database Preview"></img>
-        <div className="container-text">
-          <a href="https://github.com/cewright98/e-commerce-database" target="_blank">Express Note Taker</a>
-          <br />
-          <a href="https://github.com/cewright98/e-commerce-database" target="_blank">
-            <i className="fa fa-github"></i>
-          </a>
+        <div className="container">
+          <img src={photo6} alt="E-Commerce Database Preview"></img>
+          <div className="container-text">
+            <a href="https://github.com/cewright98/e-commerce-database" target="_blank">Express Note Taker</a>
+            <br />
+            <a href="https://github.com/cewright98/e-commerce-database" target="_blank">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
         </div>
       </div>
     </section>
